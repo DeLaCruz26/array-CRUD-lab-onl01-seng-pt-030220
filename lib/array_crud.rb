b2d8c73d08cb18ff1cdd431fb4f4c6312967c99c
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-08 09:24:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  add_element_to_start_of_array = ["Kelsey", "Dametrik", "Sebastian", "Seven"]
+  add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
